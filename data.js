48b8762fa81067c5a17dd3bed26c71bb0042db46
Ezri White <ezri@live.unc.edu>
2020-10-17 14:22:13 -0400

    added fake data

---
+++ data.js
@@ -0,0 +1,23 @@
+
+
+/**
+ * Represents a list companies
+ */
+const companies = {
+    patagonia: {
+        name: "Patagonia",
+        ethical: 10,
+        sustainable: 10,
+        minority_owned: false
+    }, forever21: {
+        name: "Forver 21",
+        ethical: 2,
+        sustainable: 3,
+        minority_owned: false
+    }, athleta: {
+        name: "Athleta",
+        ethical: 7,
+        sustainable: 9,
+        minority_owned: false
+    }
+}
--- popup.js
+++ popup.js
@@ -4,24 +4,32 @@ document.addEventListener('DOMContentLoaded', function () {
     var ethicsButton = document.getElementById('ethics');
     var minorityOwnedButton = document.getElementById('minority-owned');
     checkPageButton.addEventListener('click', function () {
-        var newURL = "http://espn.com";
-        chrome.tabs.create({ url: newURL }, function () {
+
+        chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+            let url = tabs[0].url;
+
+            chrome.tabs.create({ url: url });
 
         });
+        /*var newURL = "http://espn.com";
+        chrome.tabs.create({ url: newURL }, function () {
+
+        });*/
     }, false);
-    
+
     sustainabilityButton.addEventListener('click', function () {
         var newURL = "https://www.forbes.com/sites/ellevate/2019/10/07/why-sustainable-fashion-matters/#77a99c1771b8";
         chrome.tabs.create({ url: newURL });
     }, false);
 
-    ethicsButton.addEventListener('click', function() {
+    ethicsButton.addEventListener('click', function () {
         var newURL = "https://ethicalmadeeasy.com/what-is-ethical-fashion/";
         chrome.tabs.create({ url: newURL });
     });
 
-    minorityOwnedButton.addEventListener('click', function() {
+    minorityOwnedButton.addEventListener('click', function () {
         var newURL = "https://medium.com/juno-collective/why-helping-minority-owned-businesses-is-still-important-4475403c041b";
         chrome.tabs.create({ url: newURL });
     });
 }, false);
+
