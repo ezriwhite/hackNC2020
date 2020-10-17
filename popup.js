fd4160876f37eca82530d492325b9f03b432d89b
Ezri White <ezri@live.unc.edu>
2020-10-17 16:22:13 -0400

    added doc thing

--- popup.js
+++ popup.js
@@ -3,19 +3,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var sustainabilityButton = document.getElementById('sustainability');
     var ethicsButton = document.getElementById('ethics');
     var minorityOwnedButton = document.getElementById('minority-owned');
-    checkPageButton.addEventListener('click', function () {
 
-        chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
-            let url = tabs[0].url;
-
-            chrome.tabs.create({ url: url });
-
-        });
-        /*var newURL = "http://espn.com";
-        chrome.tabs.create({ url: newURL }, function () {
-
-        });*/
-    }, false);
 
     sustainabilityButton.addEventListener('click', function () {
         var newURL = "https://www.forbes.com/sites/ellevate/2019/10/07/why-sustainable-fashion-matters/#77a99c1771b8";
@@ -31,22 +19,36 @@ document.addEventListener('DOMContentLoaded', function () {
         var newURL = "https://medium.com/juno-collective/why-helping-minority-owned-businesses-is-still-important-4475403c041b";
         chrome.tabs.create({ url: newURL });
     });
+    checkPageButton.addEventListener('click', function () {
+
+        chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+            let url = tabs[0].url;
+
+            chrome.tabs.create({ url: url });
+
+        });
+        /*var newURL = "http://espn.com";
+        chrome.tabs.create({ url: newURL }, function () {
+
+        });*/
+    }, false);
 }, false);
 
 document.addEventListener('keypress', function (e) {
     if (e.key === 'Enter') {
         company = document.getElementById('company-name').value
+        document.getElementById('root').appendChild(document.createElement("P").appendChild(document.createTextNode(company)))
 
         // const $root = $("#root");
         // $root.append(`<p>Hello</p>`)
         // renderCompany(companies.find((c) => c.name === company))
-        if (company == "hello") {
+        /*if (company == "hello") {
             var newURL = "http://espn.com";
             chrome.tabs.create({ url: newURL });
         } else {
             var newURL = "http://yahoo.com";
             chrome.tabs.create({ url: newURL });
-        }
+        }*/
         /*var newItem = document.createElement("p");
         newItem.appendChild(document.createTextNode(company))
         document.getElementById('comp-name').insertBefore(newItem, list.childNodes[0])*/
