24ab24700ff9a8c1680188e5d35ba07dca00f757
irischien <irisiris@live.unc.edu>
2020-10-17 13:51:43 -0400

    second

--- popup.js
+++ popup.js
@@ -1,22 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
     var checkPageButton = document.getElementById('checkPage');
+    var sustainabilityButton = document.getElementById('sustainability');
+    var ethicsButton = document.getElementById('ethics');
+    var minorityOwnedButton = document.getElementById('minority-owned');
     checkPageButton.addEventListener('click', function () {
         var newURL = "http://espn.com";
-        chrome.tabs.create({ url: newURL });
-        // chrome.tabs.getSelected(null, function (tab) {
-        //     d = document;
+        chrome.tabs.create({ url: newURL }, function () {
 
-        //     var f = d.createElement('form');
-        //     f.action = 'http://gtmetrix.com/analyze.html?bm';
-        //     f.method = 'post';
-        //     var i = d.createElement('input');
-        //     i.type = 'hidden';
-        //     i.name = 'url';
-        //     i.value = tab.url;
-        //     f.appendChild(i);
-        //     d.body.appendChild(f);
-        //     f.submit();
-        // });
+        });
     }, false);
-}, false);
+    
+    sustainabilityButton.addEventListener('click', function () {
+        var newURL = "https://www.forbes.com/sites/ellevate/2019/10/07/why-sustainable-fashion-matters/#77a99c1771b8";
+        chrome.tabs.create({ url: newURL });
+    }, false);
 
+    ethicsButton.addEventListener('click', function() {
+        var newURL = "https://ethicalmadeeasy.com/what-is-ethical-fashion/";
+        chrome.tabs.create({ url: newURL });
+    });
+
+    minorityOwnedButton.addEventListener('click', function() {
+        var newURL = "https://medium.com/juno-collective/why-helping-minority-owned-businesses-is-still-important-4475403c041b";
+        chrome.tabs.create({ url: newURL });
+    });
+}, false);
