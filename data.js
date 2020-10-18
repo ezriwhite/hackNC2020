a1c703a53b1e0d99cba097aec881ae9144ada887
Ezri White <ezri@live.unc.edu>
2020-10-18 11:11:05 -0400

    added minority owned brand

--- data.js
+++ data.js
@@ -4,15 +4,16 @@
  * Represents a list companies
  */
 const companies = [
-    {name: "Patagonia", ethical: "excellent", sustainable: "excellent", minority_owned: false},
-    {name: "Reformation", ethical: "excellent",sustainable: "excellent", minority_owned: false}, 
-    {name: "Levi's", ethical: "excellent",sustainable: "excellent",minority_owned: false}, 
-    {name: "Forever 21", ethical: "poor", sustainable: "poor", minority_owned: false}, 
-    {name: "Fashion Nova", ethical: "poor", sustainable: "poor", minority_owned: false}, 
-    {name: "Gap", ethical: "average", sustainable: "average", minority_owned: false},
-    {name: "Madewell", ethical: "good", sustainable: "good", minority_owned: false},
-    {name: "Nike", ethical: "poor", sustainable: "average", minority_owned: false},
-    {name: "Adidas", ethical: "good", sustainable: "excellent", minority_owned: false},
-    {name: "H&M", ethical: "average", sustainable: "average", minority_owned: false}, 
-    {name: "Zara", ethical: "poor", sustainable: "poor", minority_owned: false}, 
+    { name: "Patagonia", ethical: "excellent", sustainable: "excellent", minority_owned: "No" },
+    { name: "Reformation", ethical: "excellent", sustainable: "excellent", minority_owned: "No" },
+    { name: "Levi's", ethical: "excellent", sustainable: "excellent", minority_owned: "No" },
+    { name: "Forever 21", ethical: "poor", sustainable: "poor", minority_owned: "No" },
+    { name: "Fashion Nova", ethical: "poor", sustainable: "poor", minority_owned: "No" },
+    { name: "Gap", ethical: "average", sustainable: "average", minority_owned: "No" },
+    { name: "Madewell", ethical: "good", sustainable: "good", minority_owned: "No" },
+    { name: "Nike", ethical: "poor", sustainable: "average", minority_owned: "No" },
+    { name: "Adidas", ethical: "good", sustainable: "excellent", minority_owned: "No" },
+    { name: "H&M", ethical: "average", sustainable: "average", minority_owned: "No" },
+    { name: "Zara", ethical: "poor", sustainable: "poor", minority_owned: "No" },
+    { name: "Lionne", ethical: "excellent", sustainable: "good", minority_owned: "Yes!" }
 ]
