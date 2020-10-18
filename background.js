d1f20ca9d138aa59c931f8247fd8d7e7a821b27b
Ezri White <ezri@live.unc.edu>
2020-10-17 23:01:13 -0400

    tab is created in background

--- background.js
+++ background.js
@@ -2,9 +2,9 @@ console.log("Atleast reached background.js")
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         var newURL = `https://google.com/search?q=${request.company}+sustainability`;
-        chrome.tabs.create({ url: newURL });
+        chrome.tabs.create({ url: newURL, active: false });
+        // hrome.tabs.executeScript()
 
-        //Construct & send a response
         sendResponse({
             response: `https://google.com/search?q=${request.company}+sustainability`
         });
